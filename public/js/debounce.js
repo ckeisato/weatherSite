d2a7889fe3d95e981d210e277da4f085c45e7b70
Kei Sato <ckeisato@gmail.com>
2016-10-04 08:29:14 -0400

    public build

---
+++ public/js/debounce.js
@@ -0,0 +1,19 @@
+// debounced function, taken from underscore
+define(function(func, wait, immediate) {
+	return {
+		debounce: function(func, wait, immediate) {
+			var timeout;
+			return function() {
+				var context = this, args = arguments;
+				var later = function() {
+					timeout = null;
+					if (!immediate) func.apply(context, args);
+				};
+				var callNow = immediate && !timeout;
+				clearTimeout(timeout);
+				timeout = setTimeout(later, wait);
+				if (callNow) func.apply(context, args);
+			};
+		}
+	}
+});
